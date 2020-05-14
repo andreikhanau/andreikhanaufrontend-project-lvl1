a05b5f3822ebf63c6f8a36f911e91fed37bf5a7f
andreikhanau <andreikhanau@yahoo.com>
2020-05-14 14:05:12 +0200

    brain-gcd2

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -5,8 +5,9 @@ const gcdEngine = () => {
     const question = `${randNum1} ${randNum2}`;
     const findSmallestNum = (num1, num2) => num1 > num2 ? num2 : num1;
     const findGreatestDiviser = (num1, num2) =>{
+      // eslint-disable-next-line for-direction
       for (let i = findSmallestNum(num1, num2); i <= findSmallestNum(num1, num2); i--) {
-        if (num1 % i === 0 && num2 % i === 0) {return i};
+        if (num1 % i === 0 && num2 % i === 0) {return i}
     }};
     const answer = findGreatestDiviser(randNum1, randNum2);
     
